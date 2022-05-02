44a9dd73a8a6131b73f0c0642eb533b91ecf11e5
Kevin Chu <14960080+imkevinchu@users.noreply.github.com>
2022-05-02 08:23:54 -0700

    highlight grid squares on mouseover

--- script.js
+++ script.js
@@ -5,9 +5,22 @@ const makeRows = (rows, cols) => {
   container.style.setProperty("--grid-cols", cols);
   for (let c = 0; c < rows * cols; c++) {
     let cell = document.createElement("div");
-    cell.innerText = c + 1;
     container.appendChild(cell).className = "grid-item";
   }
 };
 
 makeRows(16, 16);
+
+// const gridItems = document.querySelectorAll("grid-item");
+
+// gridItems.forEach((gridItem) => {
+//   gridItem.addEventListener("mouseover", (e) => {
+//     e.target.style.color = "yellow";
+//   });
+// });
+
+document.addEventListener("mouseover", (e) => {
+  if (e.target.className == "grid-item") {
+    e.target.style.backgroundColor = "yellow";
+  }
+});
